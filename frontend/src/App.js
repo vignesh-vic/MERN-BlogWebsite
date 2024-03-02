75a8d267088ac23c66bdfcca16bf46302c364806
vignesh-vic <vignesha010@gmail.com>
2024-03-02 21:44:41 +0530

    create router pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
-
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import DashBoard from './pages/DashBoard'
+import Projects from './pages/Projects'
 function App() {
   return (
-    <div className="App text-6xl bg-yellow-300">
-      hai
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/dashBoard' element={<DashBoard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/DashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashBoard() {
+  return (
+    <div>
+      dashBoard
+    </div>
+  )
+}
+
+export default DashBoard
